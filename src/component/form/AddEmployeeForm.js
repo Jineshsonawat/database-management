4b15a8dd76766b5aab51b82265e46ca5508f233a
dharmeshKochariOS <jineshsonawat9929@gmail.com>
2024-02-25 14:44:57 +0530

    Files : Form Added (Structure)

--- src/component/form/AddEmployeeForm.js
+++ src/component/form/AddEmployeeForm.js
@@ -8,9 +8,17 @@ function AddEmployeeForm() {
     email: "",
     gender: "",
     hobbies: [],
-    education: [],
-    experience: [],
-    skills: [],
+    education: [{ institute: "", pass_out_year: "" }],
+    skills: [{ name: "", experience: "" }],
+    experience: [
+      {
+        company: "",
+        project: "",
+        role: "",
+        duration_from: "",
+        duration_to: "",
+      },
+    ],
   });
   return (
     <div className="employee-add__details">
@@ -59,11 +67,95 @@ function AddEmployeeForm() {
             </div>
           </aside>
         </div>
+
         <hr />
+
         <div className="flex-row gap-7 ">
           <h2>Education :-</h2>
 
-          <aside></aside>
+          <aside className="flex-column content-start gap-1 ">
+            {formData.education.map((edu, index) => {
+              return (
+                <div className="width-100">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Name of School/College/Institute"
+                    />
+                    <input type="number" placeholder="Pass out year" />
+                  </div>
+                  {index !== 0 && <button type="button">Remove</button>}
+                </div>
+              );
+            })}
+
+            <div>
+              <button type="button">Add Education</button>
+            </div>
+          </aside>
+        </div>
+
+        <hr />
+
+        <div className="flex-row gap-7 ">
+          <h2>Skills :-</h2>
+
+          <aside className="flex-column content-start gap-1 ">
+            {formData.skills.map((edu, index) => {
+              return (
+                <div className="width-100">
+                  <div>
+                    <input type="text" placeholder="Skill Name" />
+                    <input type="number" placeholder="Experience" />
+                  </div>
+                  {index !== 0 && <button type="button">Remove</button>}
+                </div>
+              );
+            })}
+
+            <div>
+              <button type="button">Add Skill</button>
+            </div>
+          </aside>
+        </div>
+
+        <hr />
+
+        <div className="flex-row gap-7 ">
+          <h2>Experience :-</h2>
+
+          <aside className="flex-column content-start gap-1 ">
+            {formData.experience.map((edu, index) => {
+              return (
+                <div className="width-100">
+                  <div>
+                    <input type="text" placeholder="Company Name" />
+                    <input type="text" placeholder="Project Name" />
+                    <input type="text" placeholder="Role" />
+                    <input
+                      type="number"
+                      placeholder="Start Date (e.g., Jan 2021)"
+                    />
+                    <input
+                      type="number"
+                      placeholder="End Date (e.g., Nov 2021)"
+                    />
+                  </div>
+                  {index !== 0 && <button type="button">Remove</button>}
+                </div>
+              );
+            })}
+
+            <div>
+              <button type="button">Add Experience</button>
+            </div>
+          </aside>
+        </div>
+
+        <hr />
+
+        <div className="flex-row content-center">
+          <button type="submit">Submit</button>
         </div>
       </form>
     </div>
